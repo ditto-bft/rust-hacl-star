5fcb66d2a5e7aa1688b42b1dbf9f442ead4f5aa3
Tiago Carvalho <up201605259@fc.up.pt>
2020-10-26 22:46:25 +0000

    use bindgen

--- hacl-star-sys/src/lib.rs
+++ hacl-star-sys/src/lib.rs
@@ -15,42 +15,5 @@ mod libc {
     pub type c_uchar = u8;
 }
 
-#[cfg(any(
-    not(feature = "bindgen"),
-    all(feature = "bindgen", feature = "overwrite")
-))]
 mod imp;
-
-#[cfg(all(feature = "bindgen", not(feature = "overwrite")))]
-mod imp {
-    macro_rules! import {
-        ( @import $name:ident ) => {
-            pub mod $name {
-                include!(concat!(env!("OUT_DIR"), "/", stringify!($name), ".rs"));
-            }
-        };
-        ( $( pub mod $name:ident ; )* ) => {
-            $(
-                import!(@import $name);
-            )*
-        }
-    }
-
-    import!{
-        pub mod aead_poly1305;
-        pub mod chacha20poly1305;
-        pub mod salsa20;
-        pub mod chacha20;
-        pub mod poly1305;
-        pub mod hmac_sha2_256;
-        pub mod sha2_256;
-        pub mod sha2_384;
-        pub mod sha2_512;
-        pub mod ed25519;
-        pub mod curve25519;
-        pub mod hacl_policies;
-        pub mod nacl;
-    }
-}
-
 pub use imp::*;
