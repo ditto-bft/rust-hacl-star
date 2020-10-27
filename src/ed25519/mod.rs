7d30e49db3d724b4228c13c53438da2c3afc4b39
Tiago Carvalho <up201605259@fc.up.pt>
2020-10-27 01:19:59 +0000

    adjust code to our ffi

--- src/ed25519/mod.rs
+++ src/ed25519/mod.rs
@@ -1,33 +1,17 @@
-use rand_core::{ RngCore, CryptoRng };
-use hacl_star_sys as ffi;
-
+use hacl_star_sys::ffi;
 
 pub const SECRET_LENGTH: usize = 32;
 pub const PUBLIC_LENGTH: usize = 32;
-pub const SIG_LENGTH  : usize = 64;
+pub const SIG_LENGTH: usize = 64;
 
-define!{
-    pub struct SecretKey/secretkey(pub [u8; SECRET_LENGTH]);
-    pub struct PublicKey/publickey(pub [u8; PUBLIC_LENGTH]);
-    pub struct Signature/signature(pub [u8; SIG_LENGTH]);
-}
+#[derive(Clone)]
+pub struct SecretKey(pub [u8; SECRET_LENGTH]);
 
-#[inline]
-pub fn keypair<R: RngCore + CryptoRng>(mut rng: R) -> (SecretKey, PublicKey) {
-    let mut sk = [0; SECRET_LENGTH];
-    let mut pk = [0; PUBLIC_LENGTH];
+#[derive(Clone)]
+pub struct PublicKey(pub [u8; PUBLIC_LENGTH]);
 
-    rng.fill_bytes(&mut sk);
-
-    unsafe {
-        ffi::ed25519::Hacl_Ed25519_secret_to_public(
-            pk.as_mut_ptr(),
-            sk.as_ptr() as _
-        );
-    }
-
-    (SecretKey(sk), PublicKey(pk))
-}
+#[derive(Clone)]
+pub struct Signature(pub [u8; SIG_LENGTH]);
 
 impl SecretKey {
     #[inline]
@@ -36,7 +20,7 @@ impl SecretKey {
         let mut pk = [0; PUBLIC_LENGTH];
 
         unsafe {
-            ffi::ed25519::Hacl_Ed25519_secret_to_public(
+            ffi::Hacl_Ed25519_secret_to_public(
                 pk.as_mut_ptr(),
                 sk.as_ptr() as _
             );
@@ -50,7 +34,7 @@ impl SecretKey {
         let mut sig = [0; SIG_LENGTH];
 
         unsafe {
-            ffi::ed25519::Hacl_Ed25519_sign(
+            ffi::Hacl_Ed25519_sign(
                 sig.as_mut_ptr(),
                 sk.as_ptr() as _,
                 msg.as_ptr() as _,
@@ -63,11 +47,10 @@ impl SecretKey {
 }
 
 impl PublicKey {
-    pub fn verify(self, msg: &[u8], &Signature(ref sig): &Signature) -> bool {
-        let PublicKey(pk) = self;
-
+    pub fn verify(&self, msg: &[u8], &Signature(ref sig): &Signature) -> bool {
+        let PublicKey(ref pk) = self;
         unsafe {
-            ffi::ed25519::Hacl_Ed25519_verify(
+            ffi::Hacl_Ed25519_verify(
                 pk.as_ptr() as _,
                 msg.as_ptr() as _,
                 msg.len() as _,
