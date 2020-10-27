7bfb6f4d674f60597bbcb942bd054a39aa7ed6d5
Tiago Carvalho <up201605259@fc.up.pt>
2020-10-27 02:35:59 +0000

    hmm

--- hacl-star-sys/src/lib.rs
+++ hacl-star-sys/src/lib.rs
@@ -3,16 +3,4 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-#[cfg(not(all(target_arch = "wasm32", not(any(target_os = "emscripten", target_os = "wasi")))))]
-use libc;
-
-#[cfg(all(target_arch = "wasm32", not(any(target_os = "emscripten", target_os = "wasi"))))]
-mod libc {
-    pub type c_void = u8;
-    pub type c_int = i32;
-    pub type c_uint = u32;
-    pub type c_ulong = u32;
-    pub type c_uchar = u8;
-}
-
 pub mod ffi;
